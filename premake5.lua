808201a442152d1637eb7d9d2ee33204a2cbdb1c
WhoseTheNerd <whosethenerd@protonmail.com>
2021-11-05 18:08:22 +0200

    SDL lib compilation

--- premake5.lua
+++ premake5.lua
@@ -5,17 +5,56 @@ workspace "Pong"
         "Release"
     }
 
+project "SDL"
+    location "lib/SDL"
+
+    kind "StaticLib"
+    language "C"
+    cdialect "c99"
+
+    targetdir "bin/%{cfg.buildcfg}"
+
+    includedirs
+    {
+        "include/"
+    }
+
+    files 
+    {  
+        "src/**.c" 
+    }
+
+    filter "configurations:Debug"      
+        defines { "DEBUG" }      
+        symbols "On"
+    filter "configurations:Release"      
+        defines { "NDEBUG" }      
+        optimize "On"
+
 project "Pong"
     kind "ConsoleApp"
     language "C"
     cdialect "c99"
 
     targetdir "bin/%{cfg.buildcfg}"
+
+    includedirs
+    {
+        "src/",
+        "include/"
+    }
+
     files 
     { 
         "src/**.h", 
         "src/**.c" 
     }
+
+    links
+    {
+        "SDL"
+    }
+
     filter "configurations:Debug"      
         defines { "DEBUG" }      
         symbols "On"
